98100bf965bb69bfb877a20cf04790295f1783b9
Jason <jasonzhang501@gmail.com>
2023-11-28 17:50:27 -0500

    Need to style show page

--- frontend/src/components/Event/EventIndex.js
+++ frontend/src/components/Event/EventIndex.js
@@ -4,34 +4,27 @@ import EventIndexItem from './EventIndexItem';
 import { getEvents, fetchEvents } from '../../store/events';
 import './EventIndex.css';
 import NavBar from '../NavBar';
-import { useHistory } from 'react-router-dom';
 
 const EventIndex = () => {
     const dispatch = useDispatch();
     const events = useSelector(getEvents);
-    const history = useHistory();
 
     useEffect(() => {
         dispatch(fetchEvents());
     }, [dispatch]);
-
-    const redirectToEventShowPage = (eventId) => {
-        history.push(`/events/${eventId}`);
-    };
 
     return (
         <>
             <NavBar />
             <div className="event-index">
                 <div className="google-maps-container">
-
+                    
                 </div>
                 <div className="display-all-events">
                     {events.map((event) => (
                         <EventIndexItem
-                            key={event.id}
+                            key={event._id}
                             event={event}
-                            onClick={() => redirectToEventShowPage(event.id)}
                         />
                     ))}
                 </div>
