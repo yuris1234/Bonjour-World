f743c562d7e032c50eb15df48ced120061c74f42
“Eltion <ebehrami01@gmail.com>
2023-11-29 11:40:48 -0500

    Render map on events index

--- frontend/src/components/Event/EventIndex.js
+++ frontend/src/components/Event/EventIndex.js
@@ -4,7 +4,8 @@ import EventIndexItem from './EventIndexItem';
 import { getEvents, fetchEvents } from '../../store/events';
 import './EventIndex.css';
 import NavBar from '../NavBar'
-import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
+import EventsMapWrapper, { EventMap } from '../EventMap'
+
 
 
 const EventIndex = () => {
@@ -15,41 +16,20 @@ const EventIndex = () => {
         dispatch(fetchEvents());
     }, [dispatch]);
 
-    const mapContainerStyle = {
-        width: '100vw',
-        height: '100vh',
-    };
-
-    const center = {
-        lat: 7.2905715, // default latitude
-        lng: 80.6337262, // default longitude
-    };
-
-    const { isLoaded, loadError } = useLoadScript({
-        googleMapsApiKey: "AIzaSyAzrjxUj8NzVPJ8Gl4zB-_ifjNUdvBg2ew"
-      });
-    
-      if (loadError) {
-        return <div>Error loading maps</div>;
-      }
-    
-      if (!isLoaded) {
-        return <div>Loading maps</div>;
-      }
-
     return (
         <>
             <NavBar />
             <div className="event-index">
                 <div className="google-maps-container">
                 <div>
-                    <GoogleMap
+                    {/* <GoogleMap
                         mapContainerStyle={mapContainerStyle}
                         zoom={10}
                         center={center}
                     >
                         <Marker position={center} />
-                    </GoogleMap>
+                    </GoogleMap> */}
+                    <EventsMapWrapper events={events} />
                 </div>
                 </div>
                 <div className="display-all-events">
