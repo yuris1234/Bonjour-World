b6653e58de60fe5b6dfa836cf1adc84990770fcf
“Eltion <ebehrami01@gmail.com>
2023-11-29 12:14:10 -0500

    Edit map

--- frontend/src/components/Event/EventIndex.js
+++ frontend/src/components/Event/EventIndex.js
@@ -5,16 +5,42 @@ import { getEvents, fetchEvents } from '../../store/events';
 import './EventIndex.css';
 import NavBar from '../NavBar'
 import EventsMapWrapper, { EventMap } from '../EventMap'
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 
 
 
 const EventIndex = () => {
     const dispatch = useDispatch();
     const events = useSelector(getEvents);
+    const history = useHistory();
 
     useEffect(() => {
         dispatch(fetchEvents());
     }, [dispatch]);
+
+    const markerEventHandlers = {
+        click: (eventId) => {
+          // Navigate to the event's show page
+            history.push(`/events/${eventId}`);
+        },
+    };
+
+    const mapEventHandlers = {
+        click: (event) => {
+            const { latLng } = event;
+            const lat = latLng.lat();
+            const lng = latLng.lng();
+    
+          // Convert latLng to a query string
+            const queryString = new URLSearchParams({ lat, lng }).toString();
+    
+          // Navigate to the new event page with a query string
+            history.push({
+                pathname: '/new-event-page',
+                search: queryString,
+            });
+        },
+    };
 
     return (
         <>
@@ -22,14 +48,7 @@ const EventIndex = () => {
             <div className="event-index">
                 <div className="google-maps-container">
                 <div>
-                    {/* <GoogleMap
-                        mapContainerStyle={mapContainerStyle}
-                        zoom={10}
-                        center={center}
-                    >
-                        <Marker position={center} />
-                    </GoogleMap> */}
-                    <EventsMapWrapper events={events} />
+                    <EventsMapWrapper events={events} mapEventHandlers={mapEventHandlers} markerEventHandlers={markerEventHandlers} />
                 </div>
                 </div>
                 <div className="display-all-events">
