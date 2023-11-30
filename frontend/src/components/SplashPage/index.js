f50f741906b4d4abd28c3a6a478b4a6b1da0e1f5
Jason <jasonzhang501@gmail.com>
2023-11-30 15:24:48 -0500

    dropdown animation

--- frontend/src/components/SplashPage/index.js
+++ frontend/src/components/SplashPage/index.js
@@ -81,9 +81,9 @@ const SplashPage = () => {
           <div className="currentlanguage">{displayCurrentLanguage()}</div>
 
           <div className="image-div">
-            <img src={image1} className="image" alt=""></img>
-            <img src={image2} className="image" alt=""></img>
-            <img src={image3} className="image" alt=""></img>
+            <img src={image1} className="image" alt="" style={{ animationDelay: '0.5s' }} />
+            <img src={image2} className="image" alt="" style={{ animationDelay: '1s' }} />
+            <img src={image3} className="image" alt="" style={{ animationDelay: '1.5s' }} />
           </div>
 
           <div className="review">🌍 "{getRandomReview().text}"</div>
