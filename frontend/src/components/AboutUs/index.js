d521d47ae6cda7bf905897545f7ea15090660aa9
claudiaaziz <claudiahannahh@gmail.com>
2023-12-02 21:51:57 -0500

    Implement additional CSS for aboutme

--- frontend/src/components/AboutUs/index.js
+++ frontend/src/components/AboutUs/index.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Eltion from "../../static/images/img1.png";
 import Yuri from "../../static/images/img2.png";
 import Claudia from "../../static/images/img3.png";
 import Jason from "../../static/images/img1.png";
 import "./index.css";
 import Globe from "../Globe/Globe";
+import NavBar from "../NavBar";
 
 const teamMembers = [
   {
@@ -34,41 +35,80 @@ const teamMembers = [
 ];
 
 const AboutUs = () => {
+    const changingLanguages = ["en", "ch", "sp", "fr", "ar"];
+    const [currentLanguageIndex, setCurrentLanguageIndex] = useState(0);
+    useEffect(() => {
+      const intervalId = setInterval(() => {
+        setCurrentLanguageIndex(
+          (prevIndex) => (prevIndex + 1) % changingLanguages.length
+        );
+      }, 4000);
+
+      return () => clearInterval(intervalId);
+    }, []);
+
+  function displayMeetTheTeam() {
+    const currentLanguage = changingLanguages[currentLanguageIndex];
+    switch (currentLanguage) {
+      case "en":
+        return "Meet the Team";
+      case "ch":
+        return "团队见面";
+      case "sp":
+        return "Conoce al Equipo";
+      case "fr":
+        return "Rencontrez l'Équipe";
+      case "ar":
+        return "تعرف على الفريق";
+      default:
+        return "Meet the Team";
+    }
+  }
+
   return (
-    <div className="about-us-container">
-      {teamMembers.map((member, index) => (
-        <div key={index} className="team-member">
-          <img
-            src={
-              member.name === "Jason"
-                ? Jason
-                : member.name === "Claudia"
-                ? Claudia
-                : member.name === "Yuri"
-                ? Yuri
-                : Eltion
-            }
-            alt={`${member.name} - ${member.role}`}
-          />
-          <div className="member-details">
-            <p>{`${member.name} - ${member.role}`}</p>
-            <div className="social-links">
-              <a href={member.github} target="_blank" rel="noopener noreferrer">
-                GitHub
-              </a>
-              <a
-                href={member.linkedin}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                LinkedIn
-              </a>
+    <>
+      <NavBar />
+      <div className="about-us-container">
+        <div className="currentlanguage">{displayMeetTheTeam()}</div>
+
+        {teamMembers.map((member, index) => (
+          <div key={index} className="team-member">
+            <img
+              src={
+                member.name === "Jason"
+                  ? Jason
+                  : member.name === "Claudia"
+                  ? Claudia
+                  : member.name === "Yuri"
+                  ? Yuri
+                  : Eltion
+              }
+              alt={`${member.name} - ${member.role}`}
+            />
+            <div className="member-details">
+              <p>{`${member.name} - ${member.role}`}</p>
+              <div className="social-links">
+                <a
+                  href={member.github}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  GitHub
+                </a>
+                <a
+                  href={member.linkedin}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  LinkedIn
+                </a>
+              </div>
             </div>
           </div>
-        </div>
-      ))}
-      <Globe />
-    </div>
+        ))}
+        {/* <Globe /> */}
+      </div>
+    </>
   );
 };
 
