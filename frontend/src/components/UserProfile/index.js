703ce573825137663f994e534a77b4bcf677b51f
Jason <jasonzhang501@gmail.com>
2023-12-11 14:20:06 -0500

    images persist on user profile

--- frontend/src/components/UserProfile/index.js
+++ frontend/src/components/UserProfile/index.js
@@ -6,108 +6,149 @@ import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 import './index.css';
 import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
-import EmptyUser from '../Images/EmptyUser.png'
+import EmptyUser from '../Images/EmptyUser.png';
 import EventsMapWrapper from '../EventMap';
 import EventIndexItem from '../Event/EventIndexItem';
 import { fetchEvents, getRelevantEvents } from '../../store/events';
 import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
-
-// import empty-user from '../Images/empty-user.jpeg';
+import S3 from './aws.js';
 
 const UserProfile = () => {
-    const history = useHistory();
-    const dispatch = useDispatch();
-    const user = useSelector(state => state.session.user);
-    const [uploadedImage, setUploadedImage] = useState(null);
-    const [fadeIn, setFadeIn] = useState(false);
-    const events = useSelector(getRelevantEvents(user?._id));
-    const [highlightedEvent, setHighlightedEvent] = useState();
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.session.user);
+  const [uploadedImage, setUploadedImage] = useState(null);
+  const [fadeIn, setFadeIn] = useState(false);
+  const events = useSelector(getRelevantEvents(user?._id));
+  const [highlightedEvent, setHighlightedEvent] = useState();
+  const [imageLoaded, setImageLoaded] = useState(false);
 
-    useEffect(() => {
-        dispatch(getCurrentUser());
-        dispatch(fetchEvents());
-        setFadeIn(true);
-    }, []);
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const currentUserData = await getCurrentUser();
+        let profileImageUrl = currentUserData.profileImageUrl;
 
-    const handleImageChange = (event) => {
-        const selectedImage = event.target.files[0];
+        if (!profileImageUrl) {
+          const params = {
+            Bucket: 'bonjour-world',
+            Prefix: `user/${user?._id}/`,
+          };
 
-        if (selectedImage) {
-            const reader = new FileReader();
+          const data = await S3.listObjectsV2(params).promise();
 
-            reader.onloadend = () => {
-                setUploadedImage(reader.result);
-            };
+          const mostRecentImage = data.Contents[0];
 
-            reader.readAsDataURL(selectedImage);
+          if (mostRecentImage) {
+            profileImageUrl = S3.getSignedUrl('getObject', {
+              Bucket: 'bonjour-world',
+              Key: mostRecentImage.Key,
+            });
+          }
         }
+
+        setUploadedImage(profileImageUrl || EmptyUser);
+      } catch (error) {
+        console.error('Error fetching user data', error);
+      }
     };
 
-    const markerEventHandlers = {
-        click: (event) => {
-          // Navigate to the event's show page
-            history.push(`/events/${event._id}`);
-        },
-        mouseover: (event) => {
-            setHighlightedEvent(event);
-        },
-        mouseout: () => {
-            setHighlightedEvent(null);
-        },
-    };
+    dispatch(fetchEvents());
+    fetchUserData();
+    setFadeIn(true);
+  }, []);
 
+  const handleImageChange = async (event) => {
+    const selectedImage = event.target.files[0];
 
-    return (
-        <div className={`app-container ${fadeIn ? 'fade-in' : ''}`}>
-            <NavBar />
-            <div className={`profile-container ${fadeIn ? 'fade-in' : ''}`}>
-                <div className={`profile-details-div ${fadeIn ? 'fade-in' : ''}`}>
-                    <div className="profile-img-div">
+    if (selectedImage) {
+      const params = {
+        Bucket: 'bonjour-world',
+        Key: `user/${user?._id}/${selectedImage.name}`,
+        Body: selectedImage,
+        ACL: 'public-read',
+      };
 
-                        {/* <div className="profile-back-to-main-div">
-                            <button className="profile-back-to-main-button">
-                                <FontAwesomeIcon icon={faArrowLeft} style={{ fontSize: '40px', color: '#022a54f0' }} />
-                            </button>
-                        </div> */}
+      try {
+        console.log('Upload params:', params);
+        const data = await S3.upload(params).promise();
+        console.log('Upload successful:', data);
+        setUploadedImage(data.Location);
+      } catch (error) {
+        console.error('Error uploading image to S3:', error);
+      }
+    }
+  };
 
-                        <div className="profile-img-container">
-                            <img className="profile-img" alt="uploaded-user" src={EmptyUser} />
-                            <label htmlFor="imageInput" className="upload-label">
-                                Upload Image
-                                <input
-                                    id="imageInput"
-                                    type="file"
-                                    accept="image/*"
-                                    onChange={handleImageChange}
-                                    style={{ display: 'none' }}
-                                />
-                            </label>
-                        </div>
-                            
-                        <div className="profile-details">
-                            <div className="profile-label">{user?.firstName} {user?.lastName}</div>
-                            <div className="profile-label">{user?.username}</div>
-                        </div>
-                    </div>
+  const handleImageLoad = () => {
+    setImageLoaded(true);
+  };
 
-                <div className="event-container">
-                    <h1 className="event-header">{user?.firstName}'s Events</h1>
-                    <div className="display-all-events">
-                        {events?.map((event) => (
-                            <EventIndexItem
-                                key={event._id}
-                                event={event}
-                                highlightedEvent={highlightedEvent}
-                                setHighlightedEvent={setHighlightedEvent}
-                            />
-                        ))}
-                    </div>
-                </div>
-                </div>
+  const markerEventHandlers = {
+    click: (event) => {
+      history.push(`/events/${event._id}`);
+    },
+    mouseover: (event) => {
+      setHighlightedEvent(event);
+    },
+    mouseout: () => {
+      setHighlightedEvent(null);
+    },
+  };
+
+  return (
+    <div className={`app-container ${fadeIn ? 'fade-in' : ''}`}>
+      <NavBar />
+      <div className={`profile-container ${fadeIn ? 'fade-in' : ''}`}>
+        <div className={`profile-details-div ${fadeIn ? 'fade-in' : ''}`}>
+          <div className="profile-img-div">
+            <div className="profile-img-container">
+            <img
+                className={`profile-img ${imageLoaded ? 'visible' : 'hidden'}`}
+                alt="uploaded-user"
+                src={uploadedImage || EmptyUser}
+                onLoad={handleImageLoad}
+              />
+              {!imageLoaded && (
+                <div className="loading-spinner">Loading...</div>
+              )}
+              <label htmlFor="imageInput" className="upload-label">
+                Upload Image
+                <input
+                  id="imageInput"
+                  type="file"
+                  accept="image/*"
+                  onChange={handleImageChange}
+                  style={{ display: 'none' }}
+                />
+              </label>
             </div>
+
+            <div className="profile-details">
+              <div className="profile-label">
+                {user?.firstName} {user?.lastName}
+              </div>
+              <div className="profile-label">{user?.username}</div>
+            </div>
+          </div>
+
+          <div className="event-container">
+            <h1 className="event-header">{user?.firstName}'s Events</h1>
+            <div className="display-all-events">
+              {events?.map((event) => (
+                <EventIndexItem
+                  key={event._id}
+                  event={event}
+                  highlightedEvent={highlightedEvent}
+                  setHighlightedEvent={setHighlightedEvent}
+                />
+              ))}
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default UserProfile;
-
