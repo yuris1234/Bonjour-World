f0b9e47c25a939fc1b4a405802cc83151c0954e3
claudiaaziz <claudiahannahh@gmail.com>
2023-12-12 17:49:16 -0500

    Add CSS to pending btns

--- frontend/src/components/UserProfile/Notification/index.js
+++ frontend/src/components/UserProfile/Notification/index.js
@@ -1,30 +1,45 @@
-import { useDispatch } from "react-redux"
+import { useDispatch } from "react-redux";
 import { createJoinRequest } from "../../../store/events";
 import { deleteJoinRequest } from "../../../store/events";
 import { addEventJoin } from "../../../store/users";
+import "./index.css";
 
-export default function ({event}) {
-    const dispatch = useDispatch();
+export default function ({ event }) {
+  const dispatch = useDispatch();
 
-    const handleAccept = (e) => {
-        // deleting join request, adding user to event
-        dispatch(addEventJoin(e.target.value, event._id));
-        dispatch(deleteJoinRequest(event._id, e.target.value))
-    }
+  const handleAccept = (e) => {
+    // deleting join request, adding user to event
+    dispatch(addEventJoin(e.target.value, event._id));
+    dispatch(deleteJoinRequest(event._id, e.target.value));
+  };
 
-    const handleDelete = (e) => {
-        // deleting join request
-        dispatch(deleteJoinRequest(event._id, e.target.value))
-    }
+  const handleDelete = (e) => {
+    // deleting join request
+    dispatch(deleteJoinRequest(event._id, e.target.value));
+  };
 
-    return (
-        <li>
-            {event.pendingAttendees.map((pendingUser) => {
-                return (<>
-                    <button value={pendingUser} onClick={handleAccept}>Accept</button>
-                    <button value={pendingUser} onClick={handleDelete}>Deny</button>
-                </>)
-            })}
-        </li>
-    )
+  return (
+    <li>
+      {event.pendingAttendees.map((pendingUser) => {
+        return (
+            <div className="pending-btns">
+                <button
+                value={pendingUser}
+                onClick={handleAccept}
+                className="handle-accept"
+                >
+                Accept
+                </button>
+                <button
+                value={pendingUser}
+                onClick={handleDelete}
+                className="handle-delete"
+                >
+                Deny
+                </button>
+            </div>
+        );
+      })}
+    </li>
+  );
 }
