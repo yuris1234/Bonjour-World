93a49fc7b847ce29b132430e9360d3422b670c6e
claudiaaziz <claudiahannahh@gmail.com>
2023-12-11 20:02:10 -0500

    Position FilterForm

--- frontend/src/components/EventMap/Input.js
+++ frontend/src/components/EventMap/Input.js
@@ -1,32 +1,22 @@
-// const Input = ({label, ...inputProps}) => {
-
-//     return (
-//         <label className="input">
-//             {label}
-//             <input {...inputProps} />
-//         </label>
-//       );
-// }
-
-// export default Input;
-
-import React from 'react';
-
 const Input = ({ label, options, value, onChange }) => {
-  // Add the "Any" option to the beginning of the options array
-  const enhancedOptions = ['Any', ...options];
+  const enhancedOptions = ['Select Language', ...options];
 
   return (
-    <label className="input">
-      {label}
-      <select value={value} onChange={onChange}>
-        {enhancedOptions.map((option) => (
-          <option key={option} value={option === 'Any' ? '' : option}>
-            {option}
-          </option>
-        ))}
-      </select>
-    </label>
+    <div id="filter-form">
+      <label className="">
+        {label}
+        <select value={value} onChange={onChange}>
+          {enhancedOptions.map((option) => (
+            <option
+              key={option}
+              value={option === "Select Language" ? "" : option}
+            >
+              {option}
+            </option>
+          ))}
+        </select>
+      </label>
+    </div>
   );
 };
 
