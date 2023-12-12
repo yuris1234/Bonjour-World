07d0adbc859256a34e1c578d7aa37c7831a2fc4d
claudiaaziz <claudiahannahh@gmail.com>
2023-12-11 21:20:19 -0500

    Start UserGuideSection

--- frontend/src/components/UserGuideSection/index.js
+++ frontend/src/components/UserGuideSection/index.js
@@ -1,55 +1,49 @@
-import React from 'react'
+import React from "react";
 import "./index.css";
 
 const UserGuideSection = () => {
   return (
     <div className="user-guide-section">
-      MERN-Group Project Proposal: Bonjour World Language Exchange Platform
-      Background and Overview In a world increasingly interconnected, language
-      learning is a gateway to cultural understanding. Our project, "Bonjour
-      World," is a web platform designed to facilitate language exchange events
-      for both language learners and polyglots. The platform empowers users to
-      host and subscribe to language exchange events. Functionality and MVP
-      Minimum Viable Product (MVP) User Authentication: Secure user registration
-      and login system. Authentication mechanisms to protect user data and
-      ensure secure access. Event Management (CRUD): Creation, Read, Update, and
-      Delete (CRUD) functionality for hosting and managing language exchange
-      events. Intuitive event creation forms for hosts. User Profile: User
-      profiles with customizable information and preferences. Ability for users
-      to track their language learning progress and event history. Google Maps
-      Integration: Utilization of the Google Maps API to display language
-      exchange events geographically. Interactive maps for users to explore and
-      discover language exchange opportunities. Bonus Features Real-time
-      Messaging (Websockets): Implementation of Websockets for real-time
-      messaging between users. Users can choose language partners and engage in
-      language-specific conversations. Technologies The Bonjour World platform
-      will be developed using the MERN (MongoDB, Express.js, React.js, Node.js)
-      tech stack. The following technologies will be incorporated: MongoDB: A
-      NoSQL database for storing user data, event information, and other
-      relevant data. Express.js: A web application framework for Node.js, used
-      for building robust and scalable server-side applications. React.js: A
-      JavaScript library for building user interfaces, enabling dynamic and
-      interactive frontend experiences. Node.js: A JavaScript runtime
-      environment that allows server-side execution of JavaScript code, powering
-      the backend of the application. Google Maps API: For geospatial
-      representation of language exchange events. Websockets: To enable
-      real-time messaging features. Group Members and Work Breakdown Our team is
-      composed of the following members, each contributing their expertise to
-      ensure the success of the project: Team Lead (Eltion): Backend Engineer
-      (Yuri): Frontend Engineer (Claudia): Flex Engineer (Jason): Schedule
-      Monday: Yuri: Implement backend CRUD routes for creating an event. Jason
-      and Eltion: Work on frontend CRUD tasks (reducers, action creators, thunk,
-      forms, etc). Eltion will assist. Claudia: Implement CSS for authentication
-      sign-in, splash page. Eltion: Set up Google Maps API. Tuesday: Eltion:
-      Incorporate Google Maps API into CRUD functionalities. Claudia: Implement
-      CSS for forms, events index page, and show pages. Yuri: Work on backend
-      functionality for updating and deleting user. Jason: Work on frontend
-      functionality for updating and deleting user. Wednesday: Complete UI
-      design for all show pages, edit forms, and index pages. Start implementing
-      Websockets. Users can only see event details when attendance has been
-      confirmed. Thursday and Friday: Style direct message (DM) pages.
+      <p>
+        Welcome to Bonjour World, your gateway to language exchange and cultural
+        understanding. Follow these steps to get started:
+      </p>
+
+      <div className="instruction auth">
+        <h3>1. Sign Up or Log In</h3>
+        <p>
+          Create a secure account or log in to start your language learning
+          journey.
+        </p>
+      </div>
+
+      <div className="instruction event">
+        <h3>2. Explore Language Exchange Events</h3>
+        <p>
+          Use the interactive maps powered by Google Maps to discover language
+          exchange events near you.
+        </p>
+      </div>
+
+      <div className="instruction host-or-join">
+        <h3>3. Host or Join Events</h3>
+        <p>
+          Host your own language exchange events or join existing ones. Manage
+          events with ease using our intuitive event management system.
+        </p>
+      </div>
+
+      <div className="instruction customize-profile">
+        <h3>4. Customize Your Profile</h3>
+        <p>
+          Personalize your user profile with information, preferences, and track
+          your language learning progress and event history.
+        </p>
+      </div>
+
+      <p>Get ready to embark on a linguistic adventure with Bonjour World!</p>
     </div>
   );
-}
+};
 
 export default UserGuideSection;
