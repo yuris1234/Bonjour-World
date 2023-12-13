0bfaa942fb96af887051116f3a4dd1d6da64ce03
claudiaaziz <claudiahannahh@gmail.com>
2023-12-13 13:55:49 -0500

    Finialize eventindexitem & user profile eventindexitem

--- frontend/src/components/UserProfile/config.js
+++ frontend/src/components/UserProfile/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    accessKeyId: 'AKIASIXB3QQCMPAQLD26',
-    secretAccessKey: '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',
-    region: 'us-east-1',
+  accessKeyId: "AKIASIXB3QQCP22EDN5T",
+  secretAccessKey: "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",
+  region: "us-east-1",
 };
