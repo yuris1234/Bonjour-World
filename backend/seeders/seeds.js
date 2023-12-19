06ca1433cd8b9b6e91814a22b1f1544b213e6019
“Eltion <ebehrami01@gmail.com>
2023-12-19 10:13:54 -0500

    Add seeds

--- backend/seeders/seeds.js
+++ backend/seeders/seeds.js
@@ -164,7 +164,7 @@ events.push(
     title: "¡Hola Fiesta!",
     description: "Join us for an enchanting evening of language and laughter at ¡Hola Fiesta! - where the joy of Spanish communication comes alive! Immerse yourself in a vibrant atmosphere filled with friendly conversations, cultural connections, and a sprinkle of language games. Whether you're a fluent speaker or just starting your Spanish journey, this fiesta is the perfect opportunity to practice your skills in a relaxed and festive setting. Expect lively chatter, delicious bites, and an all-around good time as we celebrate the beauty of the Spanish language together. ¡Vamos a divertirnos! (Let's have fun!)",
     languages: ['English','Spanish'],
-    address: '620 West 153rd Street',
+    address: '543 5th Avenue',
     date: faker.date.future(),
     time: "20:00",
     endTime: "22:00",
@@ -178,7 +178,7 @@ events.push(
     title: "你好聚会 (Nǐ Hǎo Jùhuì)",
     description: "Experience the magic of Mandarin at 你好聚会! Join us for an evening filled with lively conversations, cultural connections, and the joy of learning Chinese. Whether you're a seasoned speaker or just getting started, this event provides a welcoming space to practice your language skills in a friendly and festive environment. Anticipate engaging discussions, delightful snacks, and the opportunity to make new friends who share your passion for the Chinese language. 一起来吧! (Come join us!)",
     languages: ['English', 'Chinese'],
-    address: '120 Riverside Dr',
+    address: '234 Chambers Street',
     date: faker.date.future(),
     time: "05:00",
     endTime: "07:30", 
@@ -192,7 +192,7 @@ events.push(
     title: "Learn Japanese with Natives",
     description: "Immerse yourself in the beauty of the Japanese language, where native speakers will guide you through conversational exercises, cultural nuances, and share firsthand insights to enhance your language learning journey. Join us for an interactive and engaging experience that brings the language to life with authenticity and warmth!",
     languages: ['English','Japanese'],
-    address: '255 West 84th Street',
+    address: '101 Liberty Street',
     date: faker.date.future(),
     time: "17:30",
     endTime: "19:00",
@@ -224,6 +224,142 @@ events.push(
     date: faker.date.future(),
     time: "19:00",
     endTime: "20:00",
+    attendees: [],
+    host: "65663fdc660cf7f22d333445"
+  })
+)
+
+events.push(
+  new Event ({
+    title: "Global Gab: A Multilingual Mixer",
+    description: "Join us for an evening of shared words and cultural exchange at our language exchange gathering. Connect with participants, explore the art of communication, and build lasting connections through the richness of language.",
+    languages: ['English','Portuguese', 'Japanese'],
+    address: '456 Wall Street',
+    date: faker.date.future(),
+    time: "15:00",
+    endTime: "16:30",
+    attendees: [],
+    host: "65663fdc660cf7f22d333445"
+  })
+)
+
+events.push(
+  new Event ({
+    title: "Polyglot Palooza: Connecting Cultures through Conversation",
+    description: "Immerse yourself in the symphony of words at our language exchange event. Connect with fellow language enthusiasts, explore the diversity of expressions, and celebrate the art of communication. Join us for a night of shared conversations!",
+    languages: ['English','Hindi', 'Arabic'],
+    address: '123 Broadway',
+    date: faker.date.future(),
+    time: "12:00",
+    endTime: "13:00",
+    attendees: [],
+    host: "65663fdc660cf7f22d333445"
+  })
+)
+
+events.push(
+  new Event ({
+    title: "Dialect Dialogues: Bridging Borders, Building Bonds",
+    description: "Join our language exchange event for an evening of cultural immersion through words. Immerse yourself in conversations that showcase the beauty of expressions, connect with language enthusiasts, and celebrate linguistic diversity.",
+    languages: ['Korean','Russian', 'Swahili'],
+    address: '789 Battery Park',
+    date: faker.date.future(),
+    time: "10:00",
+    endTime: "11:00",
+    attendees: [],
+    host: "65663fdc660cf7f22d333445"
+  })
+)
+
+events.push(
+  new Event ({
+    title: "Calling All Polyglots!",
+    description: "Join us for an evening of shared words and cultural exchange at our language exchange gathering. Connect with participants, explore the art of communication, and build lasting connections through the richness of language.",
+    languages: ['Portuguese','French'],
+    address: '212 Park Avenue',
+    date: faker.date.future(),
+    time: "14:00",
+    endTime: "15:30",
+    attendees: [],
+    host: "65663fdc660cf7f22d333445"
+  })
+)
+
+events.push(
+  new Event ({
+    title: "Calling All Polyglots!",
+    description: "Experience the magic of cross-cultural communication at our language exchange event. Engage in conversations that bridge gaps, connect with diverse perspectives, and celebrate the beauty of expressions from various corners of the world.",
+    languages: ['Chinese','Hindi', 'Japanese'],
+    address: '345 Madison Avenue',
+    date: faker.date.future(),
+    time: "19:00",
+    endTime: "20:00",
+    attendees: [],
+    host: "65663fdc660cf7f22d333445"
+  })
+)
+
+events.push(
+  new Event ({
+    title: "Calling All Polyglots!",
+    description: "Embark on a cultural voyage through language at our language exchange meetup. Connect with language enthusiasts, explore the nuances of communication, and celebrate the power of words to unite people. All are welcome!",
+    languages: ['Arabic','French'],
+    address: '123 8th Avenue',
+    date: faker.date.future(),
+    time: "9:00",
+    endTime: "10:00",
+    attendees: [],
+    host: "65663fdc660cf7f22d333445"
+  })
+)
+events.push(
+  new Event ({
+    title: "Calling All Polyglots!",
+    description: "Step into a realm of linguistic connections at our exchange gathering. Immerse yourself in conversations that bring together people from different backgrounds, celebrate cultural expressions, and build friendships through words.",
+    languages: ['German','Korean'],
+    address: '654 Jackson Avenue, Long Island City, NY 11101',
+    date: faker.date.future(),
+    time: "13:00",
+    endTime: "14:00",
+    attendees: [],
+    host: "65663fdc660cf7f22d333445"
+  })
+)
+events.push(
+  new Event ({
+    title: "Calling All Polyglots!",
+    description: "Celebrate the diversity of languages at our exchange event. Connect with like-minded individuals, explore unique expressions, and forge connections that go beyond linguistic barriers. Join us for an evening of shared words and connections!",
+    languages: ['Spanish','Japanese'],
+    address: '876 Lorimer Street',
+    date: faker.date.future(),
+    time: "18:00",
+    endTime: "19:00",
+    attendees: [],
+    host: "65663fdc660cf7f22d333445"
+  })
+)
+events.push(
+  new Event ({
+    title: "Calling All Polyglots!",
+    description: "Experience the excitement of our language exchange extravaganza. Engage in conversations that celebrate the richness of languages, connect with individuals who share your passion, and enjoy a cultural journey through words.",
+    languages: ['English','French'],
+    address: '789 Journal Square, Jersey City, NJ 07306',
+    date: faker.date.future(),
+    time: "20:00",
+    endTime: "21:00",
+    attendees: [],
+    host: "65663fdc660cf7f22d333445"
+  })
+)
+events.push(
+  new Event ({
+    title: "Calling All Polyglots!",
+    description: "Join us for a night of linguistic exploration at our language exchange meetup. Discover the beauty of expressions, connect with fellow participants, and build connections that go beyond words. All language lovers are invited!",
+    languages: ['Arabic','Swahili'],
+    address: '212 Greenpoint Avenue',
+    date: faker.date.future(),
+    time: "15:30",
+    endTime: "17:00",
     attendees: [],
     host: "65663fdc660cf7f22d333445"
   })
